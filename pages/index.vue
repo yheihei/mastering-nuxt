aa690ce96d24fcd471dd98be8ef0d0910eac38dd
Yohei Kokubo <yhei.register@gmail.com>
2021-12-04 14:50:58 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float:left; margin: 10px;">
+      <home-card :home="home"></home-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from '../data/homes.json'
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3)
+    }
+  },
+}
+</script>
